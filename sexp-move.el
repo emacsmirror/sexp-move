a225b97dfba02d021e5e4b3d91b921fc5ae5b18f
Philip Woods <elzairthesorcerer@gmail.com>
2015-09-15 09:38:52 -0400

    Added reason for creating sexp-move and Use subsection for sexp-move-backward

--- sexp-move.el
+++ sexp-move.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2015 Philip Woods
 
 ;; Author: Philip Woods <elzairthesorcerer@gmail.com>
-;; Version: 0.2.2
+;; Version: 0.2.3
 ;; Keywords: sexp
 ;; URL: https://gitlab.com/elzair/sexp-move
 
